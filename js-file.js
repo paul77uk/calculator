3f435a5b004013292baf052f7161a6803fa47c04
Paul Vickers <paul77uk@gmail.com>
2022-09-13 15:53:59 +0100

    try again

--- js-file.js
+++ js-file.js
@@ -23,52 +23,49 @@ const equals = document.querySelector('.equals-button');
 const dot = document.querySelector('.dot');
 const backButton = document.querySelector('.back-button');
 
-const add = (a, b) => (a + b); 
+const add = (a, b) => (a + b);
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = (operator, a, b) => 
-operator === '+' ? add(a, b) :
-operator === '-' ? subtract(a, b):
-operator === 'x' ? multiply(a, b) :
-operator === '/' ? divide(a, b) :
-null;
+const operate = (operator, a, b) =>
+    operator === '+' ? add(a, b) :
+        operator === '-' ? subtract(a, b) :
+            operator === 'x' ? multiply(a, b) :
+                operator === '/' ? divide(a, b) :
+                    null;
 
 const numberListener = (num) => {
     num.addEventListener('click', () => {
         if (displayValue2.includes('.')) {
-            if (num.textContent !== '.'){
+            if (num.textContent !== '.') {
                 displayValue2 += num.textContent;
                 display.textContent = displayValue2;
+                if (displayValue !== '') displayValue = calculate();
+                console.log(`displayValue = ${displayValue} ${operator} displayValue2 = ${displayValue2}`)
             }
         } else {
             displayValue2 += num.textContent;
             display.textContent = displayValue2;
+            if (displayValue !== '') displayValue = calculate();
+            console.log(`displayValue = ${displayValue} ${operator} displayValue2 = ${displayValue2}`)
         }
-        
-        
+
+
     });
 }
 
 const operatorListener = (operatorSign) => {
     operatorSign.addEventListener('click', () => {
         operator = operatorSign.textContent;
-        displayValue = displayValue2;
-        displayValue2 = "";
+        if (displayValue === '') displayValue = displayValue2;
+        display.textContent = displayValue;
+        displayValue2 = '';
+
     });
 }
 
-const calculate = () => {
-    if (displayValue == "") {
-        display.textContent = displayValue2;
-    } else {
-        display.textContent = operate(operator, Number(displayValue), Number(displayValue2))
-        console.log(display.textContent);
-        displayValue2 = operate(operator, Number(displayValue), Number(displayValue2))
-        displayValue = "";
-    }
-}
+const calculate = () => operate(operator, Number(displayValue), Number(displayValue2));
 
 numberListener(zero);
 numberListener(one);
@@ -88,7 +85,8 @@ operatorListener(multiplyButton);
 operatorListener(division);
 
 equals.addEventListener('click', () => {
-    calculate();
+    // calculate();
+    display.textContent = displayValue;
 });
 
 clear.addEventListener('click', () => {
@@ -109,3 +107,14 @@ backButton.addEventListener('click', () => {
 // operator press is saved
 // then displayValue2 is transfered to displayValue1
 // on = press calculation is made and total is saved to displayValue2
+
+// need now to do similar with operator buttons
+// calculate on operator press
+// but first time first time as displayValue will be empty.
+// we can say if empty just display displayValue2 else calculate
+// equal then just displays the calculation result, but does not calculate again.
+// and after the oprator is press the result will be in displayValue, 
+// so we will pass that to the display.textContent 
+
+// maybe need to be a total also
+//
